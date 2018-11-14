16f6f5a13dd5d1e491d4be31a9b20daed52cdc93
Ryan Graute <rrgraute@gmail.com>
2018-11-14 14:41:12 +0100

    calculate cart total price

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -18,24 +18,35 @@ namespace DrankReus_api.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public ActionResult Post([FromBody] int[] keys)
+        public ActionResult Post([FromBody] IdAmount[] productAmounts)
         {
+            System.Console.WriteLine(productAmounts);
+            int[] ids = (from p in productAmounts
+                        select p.id).ToArray();
+
             var products =
             (from p in db.Product
-            where keys.Contains(p.Id)
+            where ids.Contains(p.Id)
             select p).ToList();
             decimal totalPrice = 0.00m;
 
             foreach (var product in products)
             {
-                totalPrice += product.Price;
+                int amount = (from p in productAmounts where p.id == product.Id select p.amount).First();
+                totalPrice += product.Price * amount;
             }
                             
             return Ok(new {
-                products = products,
+                // products = products,
                 tax = Math.Round(totalPrice * 0.21m, 2, MidpointRounding.ToEven),
                 Grandtotal = totalPrice
             });
         }
+    }
+
+    public class IdAmount
+    {
+        public int id { get; set; }
+        public int amount { get; set; }
     }
 }
