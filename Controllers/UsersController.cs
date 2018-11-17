a335d3409c009aaf2651867bf388d1f2e690a8e8
Ryan Graute <rrgraute@gmail.com>
2018-11-18 00:29:09 +0100

    move AuthController functions to UserController

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using DrankReus_api.Data;
 using DrankReus_api.Models;
+using DrankReus_api.Helpers;
 
 namespace DrankReus_api.Controllers
 {
@@ -32,10 +34,32 @@ namespace DrankReus_api.Controllers
         }
 
         // POST api/users
+        [AllowAnonymous]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] User newUser)
         {
+            if(userExists(newUser.Email)) return StatusCode(409, "Gebruiker bestaat al.");
+            newUser.HashPassword();
+            newUser.Admin = false;
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            return StatusCode(201);
+        }
 
+        // POST api/users/login
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
+        public ActionResult Login([FromBody] JObject loginDetails)
+        {
+            string email = loginDetails["email"].ToString();
+            string password = loginDetails["password"].ToString();
+            if(!userExists(email)) return StatusCode(409, "Gebruiker bestaat niet");
+
+            User registeredUser = db.Users.Where(u => u.Email == email).First();
+            
+            if(!registeredUser.PasswordMatch(password)) return StatusCode(409, "Verkeerd wachtwoord");
+            return Ok(TokenHelper.generateToken(registeredUser, DateTime.Now.AddHours(1)));
         }
 
         // PUT api/users/5
@@ -43,6 +67,12 @@ namespace DrankReus_api.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+        }
+
+        private bool userExists(string email)
+        {
+            bool userExists = db.Users.Any(u => u.Email == email);
+            return userExists;
         }
 
         private User GetClaimUser(int requestId)
