5f864dc45b0ae04eaf91d7853f82bd3f3550b798
Ryan Graute <rrgraute@gmail.com>
2018-11-17 13:27:24 +0100

    Revert "Added products array to get certain products"
    This reverts commit fecc1dfd0aa90e4af0849b80090315aa5abb09fa.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,92 +11,76 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DrankReus_api.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class ProductController : Controller
-  {
-    private readonly WebshopContext db;
-
-    public ProductController(WebshopContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
     {
-      db = context;
-    }
+        private readonly WebshopContext db;
 
-    [HttpGet]
-    [Route("")]
-    public IActionResult GetFilteredProducts(
-    [FromQuery(Name = "Country")]int[] Country,
-    [FromQuery(Name = "Category")]int[] Category,
-    [FromQuery(Name = "Brand")]int[] Brand,
-    [FromQuery(Name = "index")]int page_index,
-    [FromQuery(Name = "size")]int page_size,
-    [FromQuery(Name = "products")] int[] products
-    )
-    {
-      var result = db.Product.Select(m => m);
-      if (Country.Length != 0)
-      {
-        result = result.Where(m => Country.Contains(m.CountryId.Value));
-      }
-      if (Category.Length != 0)
-      {
-        result = result.Where(m => Category.Contains(m.CategoryId.Value));
-      }
-      if (Brand.Length != 0)
-      {
-        result = result.Where(m => Brand.Contains(m.BrandId.Value));
-      }
-      if (products.Length != 0)
-      {
-        result = result.Where(p => products.Contains(p.Id));
-      }
-      return Ok(result.Select(p => new
-      {
-        p.Name,
-        p.Id,
-        p.Description,
-        p.Price,
-        p.Volume,
-        p.Url,
-        p.Alcoholpercentage,
-        p.CategoryEntity,
-        p.CountryEntity,
-        p.BrandEntity
-      }).GetPage(page_index, page_size, m => m.Id));
-    }
+        public ProductController(WebshopContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetFilteredProducts(
+        [FromQuery(Name = "Country")]int[] Country,
+        [FromQuery(Name = "Category")]int[] Category,
+        [FromQuery(Name = "Brand")]int[] Brand,
+        [FromQuery(Name = "index")]int page_index,
+        [FromQuery(Name = "size")]int page_size)
+        {
+            var result = db.Product.Select(m => m);
+            if(Country.Length != 0){
+                result = result.Where(m => Country.Contains(m.CountryId.Value));
+            }
+             if(Category.Length != 0){
+                result = result.Where(m => Category.Contains(m.CategoryId.Value));
+            }
+            if(Brand.Length != 0){
+                result = result.Where(m => Brand.Contains(m.BrandId.Value));
+            }
+            return Ok(result.Select(p => new {
+                p.Name,
+                p.Id,
+                p.Description,
+                p.Price,
+                p.Volume,
+                p.Url,
+                p.Alcoholpercentage,
+                p.CategoryEntity,
+                p.CountryEntity,
+                p.BrandEntity
+            }).GetPage(page_index,page_size, m => m.Id));
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProductById(int id){
+            var res = from p in db.Product
+                        where p.Id == id
+                        select new{
+                            p.Id,
+                            p.Description,
+                            p.Price,
+                            p.Volume,
+                            p.Url,
+                            p.Alcoholpercentage,
+                            p.CategoryEntity,
+                            p.CountryEntity,
+                            p.BrandEntity};
+            return Ok(res);
+        }
 
-    [HttpGet]
-    [Route("{id}")]
-    public IActionResult GetProductById(int id)
-    {
-      var res = from p in db.Product
-                where p.Id == id
-                select new
-                {
-                  p.Id,
-                  p.Description,
-                  p.Price,
-                  p.Volume,
-                  p.Url,
-                  p.Alcoholpercentage,
-                  p.CategoryEntity,
-                  p.CountryEntity,
-                  p.BrandEntity
-                };
-      return Ok(res);
+        [HttpGet]
+        [Route("GetPages/{page_index}/{page_size}")]
+        public IActionResult GetPageCount(int page_index, int page_size)
+        {
+            var res = db.Product.GetPage(page_index, page_size, a => a);
+            if (res == null) return NotFound();
+            return Ok(res);
+        }
     }
-
-    [HttpGet]
-    [Route("GetPages/{page_index}/{page_size}")]
-    public IActionResult GetPageCount(int page_index, int page_size)
-    {
-      var res = db.Product.GetPage(page_index, page_size, a => a);
-      if (res == null) return NotFound();
-      return Ok(res);
+    public class RefinedProduct{
+        public Product product { get; set; }
     }
-  }
-  public class RefinedProduct
-  {
-    public Product product { get; set; }
-  }
 }
