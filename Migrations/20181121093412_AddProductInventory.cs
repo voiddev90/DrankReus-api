18eb4a9a1a914bb29ec83617d77fb64b55084471
Ryan Graute <rrgraute@gmail.com>
2018-11-21 10:38:53 +0100

    Add inventory migration

---
+++ Migrations/20181121093412_AddProductInventory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrankReusapi.Migrations
+{
+    public partial class AddProductInventory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<int>(
+                name: "Inventory",
+                table: "Product",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inventory",
+                table: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "Prefix",
+                table: "Users",
+                newName: "prefix");
+        }
+    }
+}
