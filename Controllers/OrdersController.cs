96f8f605bdb00d177e4c4dcf99efba3ec04284ad
Ryan Graute <rrgraute@gmail.com>
2018-12-02 15:14:29 +0100

    set user if valid token and update address

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -28,7 +28,7 @@ namespace DrankReus_api.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> GetOrder(int id)
         {
             return "value";
         }
@@ -36,8 +36,13 @@ namespace DrankReus_api.Controllers
         // POST api/values
         [HttpPost]
         [AllowAnonymous]
-        public ActionResult Post([FromBody] OrderProducts requestOrder)
+        public async Task<ActionResult> Post([FromBody] OrderProducts requestOrder)
         {
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             Order order = new Order();
             order.OrderStatus = OrderStatusEnum.Ordered;
             order.OrderDate = new DateTime();
@@ -74,9 +79,27 @@ namespace DrankReus_api.Controllers
                 db.Update(productAmount.product);
             }
 
+
+
+            
+
+            User user = GetClaimUser();
+            if(user != null)
+            {
+                order.User = user;
+                if(requestOrder.SaveAddress)
+                {
+                    user.Street = requestOrder.Street;
+                    user.BuildingNumber = requestOrder.BuildingNumber;
+                    user.PostalCode = requestOrder.PostalCode;
+                    user.Area = requestOrder.Area;
+                    db.Update(user);
+                }
+            }
+
             db.Add(order);
-            db.SaveChanges();
-            return Ok();
+            await db.SaveChangesAsync();
+            return CreatedAtAction("GetOrder", new {id = order.Id}, order.Id);
 
         }
 
@@ -95,8 +118,12 @@ namespace DrankReus_api.Controllers
 
         private User GetClaimUser()
         {
-            string claimEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
-            return (from u in db.Users where u.Email == claimEmail select u).FirstOrDefault();
+            if(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email) != null)
+            {
+                string claimEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+                return (from u in db.Users where u.Email == claimEmail select u).FirstOrDefault();
+            }
+            return null;
         }
     }
 
@@ -122,6 +149,8 @@ namespace DrankReus_api.Controllers
         public string Area { get; set; }
         [Required]
         public orderProductAmount[] orderProductAmount { get; set; }
+
+        public bool SaveAddress { get; set; }
     }
 
     public class orderProductAmount
