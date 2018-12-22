682a185b3ec7eb13bb61a36a96be39b5ae6d8896
Ryan Graute <rrgraute@gmail.com>
2018-12-22 17:17:02 +0100

    Add ablility to add and update brand, Category and Country

--- Controllers/BrandController.cs
+++ Controllers/BrandController.cs
@@ -1,5 +1,8 @@
 using System.Linq;
+using System.Threading.Tasks;
 using DrankReus_api.Data;
+using DrankReus_api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DrankReus_api.Controllers
@@ -14,11 +17,41 @@ namespace DrankReus_api.Controllers
         }
 
         [HttpGet]
-        [Route("")]
         public IActionResult GetTableData(){
             return Ok(db.Brand.Select(m => m));
         }
 
+        [HttpPost, Authorize(Roles = "Admin")]
+        public async Task<ActionResult> AddBrand([FromBody] Brand newBrand)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Brand.Add(newBrand);
+            await db.SaveChangesAsync();
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult> UpdateBrand(int id, [FromBody] Brand updateBrand)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Brand brand = await db.Brand.FindAsync(id);
+
+            if(brand == null)
+            {
+                return NotFound();
+            }
+
+            brand.Name = updateBrand.Name;
+            db.Brand.Update(brand);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
 
     }
 }
--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -1,5 +1,8 @@
 using System.Linq;
+using System.Threading.Tasks;
 using DrankReus_api.Data;
+using DrankReus_api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DrankReus_api.Controllers
@@ -16,6 +19,38 @@ namespace DrankReus_api.Controllers
         [Route("")]
         public IActionResult GetTableData(){
             return Ok(db.Country.Select(m => m));
+        }
+
+        [HttpPost, Authorize(Roles = "Admin")]
+        public async Task<ActionResult> AddCountry([FromBody] Country newCountry)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Country.Add(newCountry);
+            await db.SaveChangesAsync();
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult> UpdateCountry(int id, [FromBody] Country UpdateCountry)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Country country = await db.Country.FindAsync(id);
+
+            if(country == null)
+            {
+                return NotFound();
+            }
+
+            country.Name = UpdateCountry.Name;
+            db.Country.Update(country);
+            await db.SaveChangesAsync();
+            return Ok();
         }
     }
 }
